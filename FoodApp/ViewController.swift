eca31e842102874c676aaf16e1fe967e8a21427d
tipalk <tinypahattuge@gmail.com>
2021-03-03 14:43:20 +0530

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Tiny Pahattuge on 2021-03-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
