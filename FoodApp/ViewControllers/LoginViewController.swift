3b1c688311377632fca5e7fd930a97adab06af42
tipalk <tinypahattuge@gmail.com>
2021-03-04 12:31:05 +0530

    commit

---
+++ FoodApp/ViewControllers/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  FoodApp
+//
+//  Created by Tiny Pahattuge on 2021-03-04.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
